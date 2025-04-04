72c4d9d8d155de11e0090a804551f18ee1e90060
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2025-04-03 22:35:29 -0500

    Estructura de Suppliers
    *Se crean las estructuras para empezar la logica del programa

---
+++ pizzeria/app/View/Components/suppliers/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Supplier List</title>
+</head>
+
+<body>
+
+    <div class="container mt-5">
+        <h1 class="mb-4">Supplier List</h1>
+
+        <a href="{{ route('suppliers.create') }}" class="btn btn-success mb-3">Add</a>
+
+        <table class="table table-striped table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Contact Info</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($suppliers as $supplier)
+                <tr>
+                    <th scope="row">{{ $supplier->id }}</th>
+                    <td>{{ $supplier->name }}</td>
+                    <td>{{ $supplier->contact_info }}</td>
+                    <td>
+                        <a href="{{ route('suppliers.edit', ['supplier' => $supplier->id]) }}" class="btn btn-info btn-sm">Edit</a>
+
+                        <form action="{{ route('suppliers.destroy', ['supplier' => $supplier->id]) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this supplier?')">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
