e5eaf25187a8e60153545fc83560e3afc48322a1
Varelaso32 <samyst2013@hotmail.com>
2025-04-04 13:50:50 -0500

    ✨ feat: Employees Add
    Se crea el create, store del controller, y el new.blade de employees

--- pizzeria/app/Http/Controllers/EmployesController.php
+++ pizzeria/app/Http/Controllers/EmployesController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Employes;
 
 class EmployesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
         $employees = DB::table('employees')
@@ -25,20 +24,39 @@ class EmployesController extends Controller
     return view('employees.index', ['employees' => $employees]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
-        //
+        $users = DB::table('users')
+            ->orderBy('name')
+            ->get();
+
+        return view('employees.new', ['users' => $users]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
-        //
+        $employee = new Employes();
+
+        $employee->user_id = $request->user_id;
+        $employee->position = $request->position;
+        $employee->identification_number = $request->identification_number;
+        $employee->salary = $request->salary;
+        $employee->hire_date = $request->hire_date;
+        $employee->save();
+
+        $employees = DB::table('employees')
+        ->join('users', 'employees.user_id', '=', 'users.id')
+        ->select(
+            'employees.*', 
+            'users.name as user_name', 
+            'users.email as user_email', 
+            'users.role as user_role'
+        )
+        ->get();
+
+    return view('employees.index', ['employees' => $employees]);
     }
 
     /**
