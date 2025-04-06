bb5eb7e2751456d939e7951d4c0d2ba626f0c0e4
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2025-04-06 11:32:29 -0500

    ✨ feat: Raw_materials Cont
    Se agrega el edit.blade.php y se finaliza el controller de si mismo

--- pizzeria/app/Http/Controllers/raw_materialsController.php
+++ pizzeria/app/Http/Controllers/raw_materialsController.php
@@ -39,6 +39,32 @@ class raw_materialsController extends Controller
     return redirect()->route('raw_materials.index')->with('success', 'Materia prima registrada exitosamente.');
     }
     
+    public function edit($id)
+    {
+        $raw_materials = raw_materials::find($id);
+
+        return view('raw_materials.edit', ['raw_material' => $raw_materials]);
+    }
+    public function update(Request $request, $id)
+    {
+        $raw_materials = raw_materials::find($id);
+
+        $raw_materials->name = $request->name;
+        $raw_materials->unit = $request->unit;
+        $raw_materials->current_stock = $request->current_stock;
+        $raw_materials->save();
+
+        $raw_materials = DB::table('raw_materials')
+        ->select('id', 'name', 'unit','current_stock', 'created_at', 'updated_at')
+        ->get();
+
+        return view('raw_materials.index',['raw_materials' => $raw_materials]); 
+    }
+    public function destroy($id)
+    {
+        raw_materials::destroy($id);
+        return redirect()->route('raw_materials.index')->with('success', 'Raw material deleted successfully.');
+    }
 
 
 }
