557e01b3632001e016b9ad8683a1818a937541b6
Escobar12345678 <nicoleandrea2005@gmail.com>
2025-04-05 21:14:58 -0500

    Se crea el create, store del controller pizza ingredientes para que deje agregar

--- pizzeria/app/Http/Controllers/PizzaIngredientController.php
+++ pizzeria/app/Http/Controllers/PizzaIngredientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\pizza_ingredient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\pizza_igredient;
@@ -22,4 +23,38 @@ class PizzaIngredientController extends Controller
     
         return view('pizza_ingredient.index', ['pizzaIngredients' => $pizzaIngredients]);
     }
+
+    public function create()
+    {
+        $pizzas = DB::table('pizzas')
+            ->orderBy('name')
+            ->get();
+    
+        $ingredients = DB::table('ingredients')
+            ->orderBy('name')
+            ->get();
+    
+        return view('pizza_ingredient.new', compact('pizzas', 'ingredients'));
+    }
+    public function store(Request $request)
+    {
+        DB::table('pizza_ingredient')->insert([
+            'pizza_id' => $request->pizza_id,
+            'ingredient_id' => $request->ingredient_id,
+        ]);
+
+        $pizzaIngredients = DB::table('pizza_ingredient')
+            ->join('pizzas', 'pizza_ingredient.pizza_id', '=', 'pizzas.id')
+            ->join('ingredients', 'pizza_ingredient.ingredient_id', '=', 'ingredients.id')
+            ->select(
+                'pizza_ingredient.*',
+                'pizzas.name as pizza_name',
+                'ingredients.name as ingredient_name'
+            )
+            ->get();
+
+        return view('pizza_ingredient.index', ['pizzaIngredients' => $pizzaIngredients]);
+    }
+    
+
 }
