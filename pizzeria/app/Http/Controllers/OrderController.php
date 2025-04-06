ce842f30adbaa7a344016768f49d1d402359cc5f
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-04-06 11:49:59 -0500

    ✨ feat: Implementar método store en OrderController para guardar órdenes

--- pizzeria/app/Http/Controllers/OrderController.php
+++ pizzeria/app/Http/Controllers/OrderController.php
@@ -67,7 +67,32 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $order = new Order();
+        $order->client_id = $request->input('client_id');
+        $order->branch_id = $request->input('branch_id');
+        $order->total_price = $request->input('total_price');
+        $order->status = $request->input('status');
+        $order->delivery_type = $request->input('delivery_type');
+        $order->delivery_person_id = $request->input('delivery_person_id');
+        $order->save();
+
+        $orders = DB::table('orders')
+            ->select([
+                'orders.*',
+                'branches.name as branch_name',
+                'emp_user.name as employee_name',
+                'cli_user.name as client_name'
+            ])
+            ->join('branches', 'orders.branch_id', '=', 'branches.id')
+            ->join('employees', 'orders.delivery_person_id', '=', 'employees.id')
+            ->join('users as emp_user', 'employees.user_id', '=', 'emp_user.id')
+            ->join('clients', 'orders.client_id', '=', 'clients.id')
+            ->join('users as cli_user', 'clients.user_id', '=', 'cli_user.id')
+            ->get();
+
+        return view('orders.index', [
+            'orders' => $orders
+        ]);
     }
 
     /**
