dbb13c279a18a8c75a46f98dc70eb7aae435a2d2
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-04-07 23:01:13 -0500

    🐛 fix(OrderExtraIngredientController): Actualizar formulario para agregar órdenes con ingredientes extra

--- pizzeria/resources/views/order_extra_ingredients/new.blade.php
+++ pizzeria/resources/views/order_extra_ingredients/new.blade.php
@@ -19,56 +19,28 @@
                 @csrf
 
                 <div class="mb-3">
-                    <label for="client_id" class="form-label">Cliente</label>
-                    <select class="form-select" id="client_id" name="client_id" required>
-                        <option selected disabled value="">Seleccione un cliente...</option>
-                        @foreach ( $clients as $client )
-                        <option value="{{ $client->id }}">{{ $client->name }}</option>
+                    <label for="order_id" class="form-label">Orden</label>
+                    <select class="form-select" id="order_id" name="order_id" required>
+                        <option selected disabled value="">Seleccione un orden...</option>
+                        @foreach ($orders as $order)
+                            <option value="{{ $order->id }}">{{ $order->id }} - {{ $order->client_name }}</option>
                         @endforeach
                     </select>
                 </div>
 
                 <div class="mb-3">
-                    <label for="branch_id" class="form-label">Sucursal</label>
+                    <label for="branch_id" class="form-label">Ingrediente extra</label>
                     <select class="form-select" id="branch_id" name="branch_id" required>
-                        <option selected disabled value="">Seleccione una sucursal...</option>
-                        @foreach ( $branches as $branch )
-                        <option value="{{ $branch->id }}">{{ $branch->name }}</option>
+                        <option selected disabled value="">Seleccione un ingrediente extra...</option>
+                        @foreach ($extra_ingredients as $extraIngredient)
+                        <option value="{{ $extraIngredient->id }}">{{ $extraIngredient->name }}</option>
                         @endforeach
                     </select>
                 </div>
 
                 <div class="mb-3">
-                    <label for="total_price" class="form-label">Precio total</label>
-                    <input type="number" min="0" class="form-control" id="total_price" name="total_price" required placeholder="Ingrese el tipo de pizza">
-                </div>
-
-                <div class="mb-3">
-                    <label for="status" class="form-label">Estado</label>
-                    <select class="form-select" id="status" name="status" required>
-                        <option selected disabled value="">Seleccione un estado...</option>
-                        <option value="pendiente">Pendiente</option>
-                        <option value="completado">Completado</option>
-                    </select>
-                </div>
-
-                <div class="mb-3">
-                    <label for="delivery_type" class="form-label">Tipo de delivery</label>
-                    <select class="form-select" id="delivery_type" name="delivery_type" required>
-                        <option selected disabled value="">Seleccione un tipo...</option>
-                        <option value="en_local">Se recoge en el local</option>
-                        <option value="a_domicilio">Domicilio</option>
-                    </select>
-                </div>
-
-                <div class="mb-3">
-                    <label for="delivery_person_id" class="form-label">Domiciliario</label>
-                    <select class="form-select" id="delivery_person_id" name="delivery_person_id">
-                        <option selected disabled value="">Seleccione un domiciliario...</option>
-                        @foreach ( $delivery_persons as $delivery_person )
-                        <option value="{{ $delivery_person->id }}">{{ $delivery_person->name }}</option>
-                        @endforeach
-                    </select>
+                    <label for="quantity" class="form-label">Cantidad</label>
+                    <input type="number" min="1" class="form-control" id="quantity" name="quantity" required placeholder="Ingrese la cantidad">
                 </div>
 
                 <div class="d-flex justify-content-between mt-4">
