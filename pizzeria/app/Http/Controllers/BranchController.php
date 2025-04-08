c3345a5e3b060364962fff92a9a9c8f05f956d7c
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-04-07 23:52:38 -0500

    ✨ feat(branch): Implementar métodos edit, update y destroy en BranchController

--- pizzeria/app/Http/Controllers/BranchController.php
+++ pizzeria/app/Http/Controllers/BranchController.php
@@ -55,7 +55,11 @@ class BranchController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $branch = Branch::find($id);
+
+        return view('branches.edit', [
+            'branch' => $branch
+        ]);
     }
 
     /**
@@ -63,7 +67,14 @@ class BranchController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $branch = Branch::find($id);
+        $branch->name = $request->input('name');
+        $branch->address = $request->input('address');
+        $branch->save();
+
+        return view('branches.index', [
+            'branches' => Branch::all()
+        ]);
     }
 
     /**
@@ -71,6 +82,11 @@ class BranchController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $branch = Branch::find($id);
+        $branch->delete();
+
+        return view('branches.index', [
+            'branches' => Branch::all()
+        ]);
     }
 }
