16435214cdd4b6b28e756bc08ef46d7f8f3715ff
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-04-08 00:13:06 -0500

    ✨ feat(branch): Crear vista para editar sucursal

---
+++ pizzeria/resources/views/branches/edit.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar sucursal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+
+        <div class="d-flex justify-content-between align-items-center mb-4">
+            <h1 class="text-primary">Editar sucursal</h1>
+        </div>
+
+        <form method="POST" action="{{ route('branches.update', ['branch' => $branch->id]) }}">
+            @method('put')
+            @csrf
+
+            <div class="mb-3">
+                <label for="name" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="name" name="name" required
+                    placeholder="Ingrese el nombre de la sucursal" value="{{ $branch->name }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="address" class="form-label">Dirección</label>
+                <input type="text" class="form-control" id="address" name="address" required
+                    placeholder="Ingrese el nombre de la sucursal" value="{{ $branch->address }}">
+            </div>
+
+            <div class="mt-3">
+                <button type="submit" class="btn btn-primary">Actualizar</button>
+                <a href="{{ route('branches.index') }}" class="btn btn-warning">Cancelar</a>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
