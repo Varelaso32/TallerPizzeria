bca3835fec2cb24df2f8ef7923e2cf94d4dac7f2
Varelaso32 <samyst2013@hotmail.com>
2025-05-25 00:07:27 -0500

    ✨ feat: Se crean rutas para users

--- pizzeria/routes/api.php
+++ pizzeria/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\UsersController;
 use App\Http\Controllers\api\OrdenController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,8 +9,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Rutas de orders
 Route::get('/orders', [OrdenController::class, 'index'])->name('orders');
 Route::post('/orders', [OrdenController::class, 'store'])->name('orders.store');
 Route::get('/orders/{comuna}', [OrdenController::class, 'show'])->name('orders.show');
 Route::put('/orders/{comuna}', [OrdenController::class, 'update'])->name('orders.update');
 Route::delete('/orders/{comuna}', [OrdenController::class, 'destroy'])->name('orders.destroy');
+
+//Rutas de users
+Route::get('/users', [UsersController::class, 'index'])->name('users');
+Route::post('/users', [UsersController::class, 'store'])->name('users.store');
+Route::get('/users/{user}', [UsersController::class, 'show'])->name('users.show');
+Route::put('/users/{user}', [UsersController::class, 'update'])->name('users.update');
+Route::delete('/users/{user}', [UsersController::class, 'destroy'])->name('users.destroy');
