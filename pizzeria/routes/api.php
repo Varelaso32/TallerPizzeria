e76d75696c6391abadf6b0dbbe775e22052ca9e6
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-05-25 19:37:21 -0500

    ✨ feat: Agregar controlador de orderPizza y se organizan rutas

--- pizzeria/routes/api.php
+++ pizzeria/routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
-use App\Http\Controllers\api\UsersController;
+use App\Http\Controllers\api\BranchController;
 use App\Http\Controllers\api\ClientsController;
-use App\Http\Controllers\api\OrderController;
-use App\Http\Controllers\api\PizzaRawMaterialController;
 use App\Http\Controllers\api\EmployesController;
+use App\Http\Controllers\api\ExtraIngredientController;
+use App\Http\Controllers\api\OrderController;
+use App\Http\Controllers\api\OrderExtraIngredientController;
+use App\Http\Controllers\api\OrderPizzaController;
+use App\Http\Controllers\api\PizzaIngredientController;
+use App\Http\Controllers\api\UsersController;
+use App\Http\Controllers\api\PizzaRawMaterialController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,11 +19,11 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // Rutas de branchs
-Route::get('/branchs', [OrderController::class, 'index'])->name('branch');
-Route::post('/branchs', [OrderController::class, 'store'])->name('branch.store');
-Route::get('/branchs/{branch}', [OrderController::class, 'show'])->name('branch.show');
-Route::put('/branchs/{branch}', [OrderController::class, 'update'])->name('branch.update');
-Route::delete('/branchs/{branch}', [OrderController::class, 'destroy'])->name('orders.destroy');
+Route::get('/branchs', [BranchController::class, 'index'])->name('branch');
+Route::post('/branchs', [BranchController::class, 'store'])->name('branch.store');
+Route::get('/branchs/{branch}', [BranchController::class, 'show'])->name('branch.show');
+Route::put('/branchs/{branch}', [BranchController::class, 'update'])->name('branch.update');
+Route::delete('/branchs/{branch}', [BranchController::class, 'destroy'])->name('orders.destroy');
 
 // Rutas de clients
 Route::get('/clients', [ClientsController::class, 'index'])->name('clients');
@@ -35,11 +40,11 @@ Route::put('/employees/{employee}', [EmployesController::class, 'update'])->name
 Route::delete('/employees/{employee}', [EmployesController::class, 'destroy'])->name('employees.destroy');
 
 // Rutas de extra_ingredients
-Route::get('/extra_ingredients', [OrderController::class, 'index'])->name('extra_ingredients');
-Route::post('/extra_ingredients', [OrderController::class, 'store'])->name('extra_ingredients.store');
-Route::get('/extra_ingredients/{extra_ingredient}', [OrderController::class, 'show'])->name('extra_ingredients.show');
-Route::put('/extra_ingredients/{extra_ingredient}', [OrderController::class, 'update'])->name('extra_ingredients.update');
-Route::delete('/extra_ingredients/{extra_ingredient}', [OrderController::class, 'destroy'])->name('extra_ingredients.destroy');
+Route::get('/extra_ingredients', [ExtraIngredientController::class, 'index'])->name('extra_ingredients');
+Route::post('/extra_ingredients', [ExtraIngredientController::class, 'store'])->name('extra_ingredients.store');
+Route::get('/extra_ingredients/{extra_ingredient}', [ExtraIngredientController::class, 'show'])->name('extra_ingredients.show');
+Route::put('/extra_ingredients/{extra_ingredient}', [ExtraIngredientController::class, 'update'])->name('extra_ingredients.update');
+Route::delete('/extra_ingredients/{extra_ingredient}', [ExtraIngredientController::class, 'destroy'])->name('extra_ingredients.destroy');
 
 // Rutas de orders
 Route::get('/orders', [OrderController::class, 'index'])->name('orders');
@@ -49,18 +54,25 @@ Route::put('/orders/{order}', [OrderController::class, 'update'])->name('orders.
 Route::delete('/orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
 
 // Rutas de order_extra_ingredients
-Route::get('/order_extra_ingredients', [OrderController::class, 'index'])->name('order_extra_ingredients');
-Route::post('/order_extra_ingredients', [OrderController::class, 'store'])->name('order_extra_ingredients.store');
-Route::get('/order_extra_ingredients/{order_extra_ingredient}', [OrderController::class, 'show'])->name('order_extra_ingredients.show');
-Route::put('/order_extra_ingredients/{order_extra_ingredient}', [OrderController::class, 'update'])->name('order_extra_ingredients.update');
-Route::delete('/order_extra_ingredients/{order_extra_ingredient}', [OrderController::class, 'destroy'])->name('order_extra_ingredients.destroy');
+Route::get('/order_extra_ingredients', [OrderExtraIngredientController::class, 'index'])->name('order_extra_ingredients');
+Route::post('/order_extra_ingredients', [OrderExtraIngredientController::class, 'store'])->name('order_extra_ingredients.store');
+Route::get('/order_extra_ingredients/{order_extra_ingredient}', [OrderExtraIngredientController::class, 'show'])->name('order_extra_ingredients.show');
+Route::put('/order_extra_ingredients/{order_extra_ingredient}', [OrderExtraIngredientController::class, 'update'])->name('order_extra_ingredients.update');
+Route::delete('/order_extra_ingredients/{order_extra_ingredient}', [OrderExtraIngredientController::class, 'destroy'])->name('order_extra_ingredients.destroy');
+
+// Rutas de order_pizzas
+Route::get('/order_pizzas', [OrderPizzaController::class, 'index'])->name('order_pizzas');
+Route::post('/order_pizzas', [OrderPizzaController::class, 'store'])->name('order_pizzas.store');
+Route::get('/order_pizzas/{order_pizza}', [OrderPizzaController::class, 'show'])->name('order_pizzas.show');
+Route::put('/order_pizzas/{order_pizza}', [OrderPizzaController::class, 'update'])->name('order_pizzas.update');
+Route::delete('/order_pizzas/{order_pizza}', [OrderPizzaController::class, 'destroy'])->name('order_pizzas.destroy');
 
 // Rutas de pizza_ingredients
-Route::get('/pizza_ingredients', [OrderController::class, 'index'])->name('pizza_ingredients');
-Route::post('/pizza_ingredients', [OrderController::class, 'store'])->name('pizza_ingredients.store');
-Route::get('/pizza_ingredients/{pizza_ingredient}', [OrderController::class, 'show'])->name('pizza_ingredients.show');
-Route::put('/pizza_ingredients/{pizza_ingredient}', [OrderController::class, 'update'])->name('pizza_ingredients.update');
-Route::delete('/pizza_ingredients/{pizza_ingredient}', [OrderController::class, 'destroy'])->name('pizza_ingredients.destroy');
+Route::get('/pizza_ingredients', [PizzaIngredientController::class, 'index'])->name('pizza_ingredients');
+Route::post('/pizza_ingredients', [PizzaIngredientController::class, 'store'])->name('pizza_ingredients.store');
+Route::get('/pizza_ingredients/{pizza_ingredient}', [PizzaIngredientController::class, 'show'])->name('pizza_ingredients.show');
+Route::put('/pizza_ingredients/{pizza_ingredient}', [PizzaIngredientController::class, 'update'])->name('pizza_ingredients.update');
+Route::delete('/pizza_ingredients/{pizza_ingredient}', [PizzaIngredientController::class, 'destroy'])->name('pizza_ingredients.destroy');
 
 // Rutas de pizza raw materials
 Route::get('/pizza-raw-materials', [PizzaRawMaterialController::class, 'index'])->name('pizza-raw-materials');
